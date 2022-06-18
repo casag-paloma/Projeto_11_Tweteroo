70bcff5b3439a6697cc090eeb230cb603af3973a
Paloma <casagrande.paloma@gmail.com>
2022-06-18 18:39:00 -0300

    feat post rota sign-up

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-import express from "express";
-
+import { sign } from "crypto";
+import express from "express"; 
 
 const app = express();
-
+app.use(express.json());
 console.log('tá funcionando');
+const users = []
+
+app.post('/sign-up', (request, response) => {
+    users.push(request.body);
+    response.send('OK')
+})
+
+app.get('/sign-up', (req, res) =>{
+    res.send(users);
+})
 
 app.listen(5000);
 
