67e2319144eec77f6f40a34826d78e905ee22ee5
Paloma Casagrande <casagrande.paloma@gmail.com>
2022-06-19 14:16:12 -0300

    'feat get rota '/tweets''

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"; 
+import cors from "cors";
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 console.log('tá funcionando');
 const users = [];
@@ -14,6 +16,11 @@ server.post('/sign-up', (request, response) => {
 server.post('/tweets', (request, response) => {
     tweets.push(request.body)
     response.send('OK')
+})
+
+server.get('/tweets', (request, response) => {
+    const lastTweets = tweets.slice(-10)
+    response.send(lastTweets)
 })
 
 server.listen(5000);
